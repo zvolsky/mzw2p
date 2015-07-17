348fc8e660aa43d7aad8b2e97db9ae50f4124341
zvolsky <zvolsky@seznam.cz>
2015-07-17 09:08:18 +0200

    form method

--- mzw2p.py
+++ mzw2p.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
 
+from gluon.html import URL
+from gluon.sqlhtml import SQLFORM
+
+
 def Q(txt, cnt=None):
     if cnt is None or '%d' not in txt:
         return txt
     return txt % cnt
+
+def form(tbl, **kwargs):
+    next = kwargs.pop('next', URL('default', 'index'))
+    form = SQLFORM(tbl, **kwargs)
+    if form.process().accepted:
+        redirect(next)
+    return form
